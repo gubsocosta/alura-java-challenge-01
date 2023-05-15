7caf0c81b5a18eaa61ea6cfb918e4c5d5a6466b8
Gabriel Costa <gubsocosta@gmail.com>
2023-05-15 17:36:29 -0300

    :sparkles: feat: create menu

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Challenge {
     public static void main(String[] args) {
@@ -11,5 +12,26 @@ public class Challenge {
         System.out.printf("Account type: %s%n", accountType);
         System.out.printf("Total account balance: $ %.2f%n", balance);
         System.out.println("**************************************");
+
+        String menu = """
+                Type your option:
+                                
+                1 - See the balance
+                2 - Deposit
+                3 - Withdraw
+                4 - Exit
+                """;
+        Scanner keyboardReader = new Scanner(System.in);
+        int option = 0;
+        while (option != 4) {
+            System.out.println(menu);
+            option = keyboardReader.nextInt();
+            switch (option) {
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
     }
 }
